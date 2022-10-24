91c50182af84f32459ced5bec2f1412da4a951a1
leirn <laurent@vromman.org>
2022-10-24 18:41:06 +0200

    Use swap_bytes property

--- src/game/utils.rs
+++ src/game/utils.rs
@@ -1,11 +1,9 @@
-pub fn swap16( value:u16) -> u16
-{
+pub fn swap16(value: u16) -> u16 {
     // SDL_Swap16(value) not found in crate interface
-    value << 8 | value >> 8
+    value.swap_bytes()
 }
 
-pub fn convert_16le( value: u16) -> u16
-{
+pub fn convert_16le(value: u16) -> u16 {
     // SDL_SwapLE16(value) not found in crate interface
-    value << 8 | value >> 8
+    value.swap_bytes()
 }
